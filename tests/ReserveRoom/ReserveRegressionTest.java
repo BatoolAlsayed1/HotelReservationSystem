0ea5967df2efbb3eaae03c4f5318dacd14c1c5d6
Batool Sayed Ebrahim <202109262@stu.uob.edu.bh>
2025-04-04 00:14:27 +0300

    Automated Testing using Randoop

---
+++ tests/ReserveRoom/ReserveRegressionTest.java
@@ -0,0 +1,8 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ ReserveRegressionTest0.class, ReserveRegressionTest1.class, ReserveRegressionTest2.class })
+public class ReserveRegressionTest {
+}
+
